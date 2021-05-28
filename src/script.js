11af8a37c2be21a486ba975772eeccaf34ddd073
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-05-28 11:28:51 +0100

    Reworked Nav Bar and Refactored code

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 const btn = document.getElementById("hamburger-btn");
 const mainNav = document.getElementById("navbarScroll");
+const mainContainer = document.querySelector(".main_container");
 
 function toggleBtn() {
   mainNav.classList.toggle("hide");
+  mainContainer.classList.toggle("now");
+  console.log(mainContainer);
+  console.log(mainNav);
 }
 
 btn.addEventListener("click", toggleBtn);

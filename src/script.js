b1009e38d2985d415d4abfeff7196841e696c4ce
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-05-29 12:59:39 +0100

    Fixed Carousel position, and styled buttons & styled card titles

--- src/script.js
+++ src/script.js
@@ -5,8 +5,6 @@ const mainContainer = document.querySelector(".main_container");
 function toggleBtn() {
   mainNav.classList.toggle("hide");
   mainContainer.classList.toggle("now");
-  console.log(mainContainer);
-  console.log(mainNav);
 }
 
 btn.addEventListener("click", toggleBtn);

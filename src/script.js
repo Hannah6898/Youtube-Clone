037459b83dfd787185bfeab9b93fa7c73a4ef66a
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-05-31 19:54:12 +0100

    Corrected vertical nav bar height, added links to vertical nav bar

--- src/script.js
+++ src/script.js
@@ -10,3 +10,10 @@ function toggleBtn() {
 }
 
 btn.addEventListener("click", toggleBtn);
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});

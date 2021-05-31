2fafb9266f177d95797aca5d0bf3bd9370b28641
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-05-31 17:55:31 +0100

    Added styling and java script to main container

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById("hamburger-btn");
 const mainNav = document.getElementById("navbarScroll");
 const mainContainer = document.querySelector(".main_container");
+const carousel = document.querySelector(".carousel");
 
 function toggleBtn() {
   mainNav.classList.toggle("hide");
   mainContainer.classList.toggle("now");
+  carousel.classList.toggle("expand");
 }
 
 btn.addEventListener("click", toggleBtn);

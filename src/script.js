adec34851d342db936d027906db7731232500dad
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-04 14:46:15 +0100

    Refactored code for Main container. Imported updated icons.

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById("hamburger-btn");
 const mainNav = document.getElementById("navbarScroll");
 const mainContainer = document.querySelector(".main_container");
-const carousel = document.querySelector(".carousel");
+const categories = document.querySelector(".categories");
 
 function toggleBtn() {
   mainNav.classList.toggle("hide");
   mainContainer.classList.toggle("now");
-  carousel.classList.toggle("expand");
+  categories.classList.toggle("expand");
 }
 
 btn.addEventListener("click", toggleBtn);

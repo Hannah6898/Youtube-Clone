373333c6840a4f3ebfbd81eecd63b5426768b7a5
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-06-05 12:43:08 +0100

    Inputted images for youtube thumbnails and avatars. Added event listeners to buttons. Refactored code

--- src/script.js
+++ src/script.js
@@ -1,15 +1,34 @@
-const btn = document.getElementById("hamburger-btn");
+const hamburgerBtn = document.getElementById("hamburger-btn");
 const mainNav = document.getElementById("navbarScroll");
 const mainContainer = document.querySelector(".main_container");
 const categories = document.querySelector(".categories");
+const closeBtn = document.querySelector(".video-section-title-close");
+const newsSection = document.querySelector(".news-section");
+const recommendationMsg = document.querySelector(".message");
+const undoLink = document.querySelector(".link");
 
-function toggleBtn() {
+function toggleBtn(event) {
+  event.preventDefault();
   mainNav.classList.toggle("hide");
   mainContainer.classList.toggle("now");
   categories.classList.toggle("expand");
 }
 
-btn.addEventListener("click", toggleBtn);
+function closeSection(event) {
+  event.preventDefault();
+  newsSection.classList.toggle("hide");
+  recommendationMsg.classList.toggle("show");
+}
+
+function openSection(event) {
+  event.preventDefault();
+  newsSection.classList.remove("hide");
+  recommendationMsg.classList.remove("show");
+}
+
+hamburgerBtn.addEventListener("click", toggleBtn);
+closeBtn.addEventListener("click", closeSection);
+undoLink.addEventListener("click", openSection);
 
 var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="tooltip"]')
